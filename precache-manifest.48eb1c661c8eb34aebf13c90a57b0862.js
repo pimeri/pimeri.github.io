6a6f78f42902dff83b5420967ae0aff08e8e1d43
pimeri <pablo.imeri@gmail.com>
2019-03-22 18:35:32 -0700

    initial

---
+++ precache-manifest.48eb1c661c8eb34aebf13c90a57b0862.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e9f994ebff2c4bd0cff8",
+    "url": "/static/js/main.098a6c1c.chunk.js"
+  },
+  {
+    "revision": "3b85c26e0ddf18daa674",
+    "url": "/static/js/2.06cae23d.chunk.js"
+  },
+  {
+    "revision": "e9f994ebff2c4bd0cff8",
+    "url": "/static/css/main.0cf5f569.chunk.css"
+  },
+  {
+    "revision": "b9d35d322e7ad2ffb1cecf4434282e20",
+    "url": "/index.html"
+  }
+];
