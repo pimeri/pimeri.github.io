dfcd1342c6ef6d3b4e89df6fc55161094ebf2b29
pimeri <pablo.imeri@gmail.com>
2019-03-23 18:46:34 -0700

    added social media links

--- src/components/ProfilePicture/ProfilePicture.js
+++ src/components/ProfilePicture/ProfilePicture.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import pic from './ProfilePicture.png'
 import './ProfilePicture.css';
+// import '../../../public/reset.css';
 
+const style = {
+	color: "#6b727f",
+	"fontSize": "250%",
+	// "marginLeft":"10px"
+}
 class ProfilePicture extends React.Component {
 
 
@@ -9,14 +15,40 @@ class ProfilePicture extends React.Component {
 	render() {
 
 		return (
-			<img 	className="profile-pic" 	
-					src={pic}
+			<div className="profile-container">
+				<img className="profile-pic" 	
+					 src={pic}
+					 alt="">  
+				</img>
+				<ul className="icon-container">
+					<li className="links">
+						<a 	
+							href="https://www.linkedin.com/in/pablo-imeri">
+							<i 
+						className="fab fa-linkedin"
+						style={style}>
+					</i> 
+						</a>
+
+					</li>
+					<li className="links">
+						<a 
 					
-					alt="">  
-			</img>
+					href="https://www.github.com/pimeri" >
+					<i
+						className="fab fa-github-square"
+						style={style} >
+					</i>
+				</a>
+					</li>
+					
+				</ul>
+			</div>
 		)
 		
 	}
 }
+
+
 
 export default ProfilePicture;
