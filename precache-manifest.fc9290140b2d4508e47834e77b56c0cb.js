f59216249e37c54b3413c8db4871ca30d3709e19
pimeri <pablo.imeri@gmail.com>
2019-03-25 16:01:50 -0700

    file restructure

---
+++ precache-manifest.fc9290140b2d4508e47834e77b56c0cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1861e83f700545ad3e9a950157bbfdcc",
+    "url": "/static/media/ProfilePicture.1861e83f.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "631c4183782be8934c3d",
+    "url": "/static/js/main.eefe0d05.chunk.js"
+  },
+  {
+    "revision": "814a5bd2e0a2297a4e48",
+    "url": "/static/js/2.ed077c43.chunk.js"
+  },
+  {
+    "revision": "631c4183782be8934c3d",
+    "url": "/static/css/main.b655e8b3.chunk.css"
+  },
+  {
+    "revision": "34748284856f93d843eb63bae01c8d89",
+    "url": "/index.html"
+  }
+];
